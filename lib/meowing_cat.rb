07a8d0b2281ef8ef01fcab3bef82390fd1d2ac02
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-26 21:04:43 -0500

    commit tree c3a9ba2c1a1612e265a97a8e98b9e1b1b836616c

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
 ## code your solution here. 
+class Cat
+attr_accessor :name
+
+def meow
+    puts "meow!"
+end
+
+end
+
+# Cat
+#   instantiates a new cat
+#   receives attr_accessor
+#   has a name
+#   is able to meow
+
+# Finished in 0.01191 seconds (files took 0.12586 seconds to load)
+# 4 examples, 0 failures
